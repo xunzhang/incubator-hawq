2721dcee2d5b2ff7622b620dfa4d4ac5691c9305
xunzhang <xunzhangthu@gmail.com>
2016-05-11 10:23:04 +0800

    HAWQ-711. Bugfix for libyarn/libhdfs3 build depends, build googletest when make

--- depends/libhdfs3/CMake/FindGoogleTest.cmake
+++ depends/libhdfs3/CMake/FindGoogleTest.cmake
@@ -4,25 +4,35 @@ find_path(GoogleTest_INCLUDE_DIR gtest/gtest.h
           NO_DEFAULT_PATH
           PATHS
           "/usr/local/include"
-          "/usr/include")
+          "/usr/include"
+          "${PROJECT_SOURCE_DIR}/../thirdparty/googletest/googletest/include")
+
+find_path(GoogleMock_INCLUDE_DIR gmock/gmock.h
+          NO_DEFAULT_PATH
+          PATHS
+          "/usr/local/include"
+          "/usr/include"
+          "${PROJECT_SOURCE_DIR}/../thirdparty/googletest/googlemock/include")
 
 find_library(Gtest_LIBRARY
              NAMES libgtest.a
              HINTS
              "/usr/local/lib"
-             "/usr/lib")
+             "/usr/lib"
+             "${PROJECT_SOURCE_DIR}/../thirdparty/googletest/build/googlemock")
 
 find_library(Gmock_LIBRARY
              NAMES libgmock.a
              HINTS
              "/usr/local/lib"
-             "/usr/lib")
+             "/usr/lib"
+             "${PROJECT_SOURCE_DIR}/../thirdparty/googletest/build/googlemock/gtest")
 
 message(STATUS "Find GoogleTest include path: ${GoogleTest_INCLUDE_DIR}")
 message(STATUS "Find Gtest library path: ${Gtest_LIBRARY}")
 message(STATUS "Find Gmock library path: ${Gmock_LIBRARY}")
 
-set(CMAKE_REQUIRED_INCLUDES ${GoogleTest_INCLUDE_DIR})
+set(CMAKE_REQUIRED_INCLUDES ${GoogleTest_INCLUDE_DIR} ${GoogleMock_INCLUDE_DIR})
 set(CMAKE_REQUIRED_LIBRARIES ${Gtest_LIBRARY} ${Gmock_LIBRARY})
 set(CMAKE_REQUIRED_FLAGS)
 check_cxx_source_runs("
